0a6259424e5960ba888b15a8fa56f1e7b09d0a20
Cleoner Pietralonga <cleonerp@gmail.com>
2023-11-29 23:13:17 -0300

    changed library name

--- examples/example2.py
+++ examples/example2.py
@@ -3,11 +3,10 @@
 
 from logicbit.quine_mccluskey import *
 
-trueTable = ['0000','0001','0100','0101','1111']
-#trueTable = ['0011','0000','0110','1011']
+trueTable = ['0000', '0001', '0100', '0101', '1111']
 
-quine_mcCluskey = Quine_mcCluskey(trueTable)
-quine_mcCluskey.Compute()
+qmc = Quine_McCluskey(trueTable)
+qmc.Compute()
 
 def v(a1, a2, a3, a4): # antes
     return bool(((not a1) and (not a2) and (not a3) and (not a4)) or ((not a1) and (not a2) and (not a3) and a4) or (
@@ -18,4 +17,4 @@ def v(a1, a2, a3, a4): # depois
     return bool((a1 and a2 and a3 and a4) or ((not a1) and (not a3)))
 
 for i in range(16):
-    print(bin(5)[2:].zfill(4), v((i >> 3) & 0x1, (i >> 2) & 0x1, (i >> 1) & 0x1, (i >> 0) & 0x1))
+    print(qmc.Bin2(i,4), v((i >> 3) & 0x1, (i >> 2) & 0x1, (i >> 1) & 0x1, (i >> 0) & 0x1))
